5629ca1cc48ed0378126ce418778f4c3bf0c8505
Alevarr <alevar03@mail.ru>
2023-07-28 23:47:37 +0300

    Build a navbar

--- src/App.tsx
+++ src/App.tsx
@@ -10,8 +10,8 @@ function App() {
           lg: `"nav nav" "aside main"`,
         }}
       >
-        <GridItem area={"nav"} bg={"coral"}>
-          Nav
+        <GridItem area={"nav"}>
+          <NavBar></NavBar>
         </GridItem>
 
         <Show above="lg">
