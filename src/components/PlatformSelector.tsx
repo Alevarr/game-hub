d45ef07a3f94770423d20e17572bd6601bb90b40
Alevarr <alevar03@mail.ru>
2023-08-04 17:23:37 +0300

    Added order by selector

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -20,7 +20,7 @@ const PlatformSelector = ({ onSelectPlatform, selectedPlatform }: Props) => {
   if (error) return null;
   return (
     <Menu>
-      <MenuButton as={Button} rightIcon={<BsChevronDown />} ml="16px">
+      <MenuButton as={Button} rightIcon={<BsChevronDown />}>
         {selectedPlatform?.name || "Platforms"}
       </MenuButton>
       <MenuList>
