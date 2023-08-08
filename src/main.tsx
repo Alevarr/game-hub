b3d9115f93826f1addc73db1cc4d597833f0a836
Alevarr <alevar03@mail.ru>
2023-08-08 20:09:30 +0300

    Added react query with devtools

--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,21 @@ import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import theme from "./theme";
 import App from "./App.tsx";
 import "./index.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const client = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
-      <ColorModeScript
-        initialColorMode={theme.config.initialColorMode}
-      ></ColorModeScript>
-      <App />
+      <QueryClientProvider client={client}>
+        <ColorModeScript
+          initialColorMode={theme.config.initialColorMode}
+        ></ColorModeScript>
+        <App />
+        <ReactQueryDevtools />
+      </QueryClientProvider>
     </ChakraProvider>
   </React.StrictMode>
 );
